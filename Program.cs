6023080cd34e79d95e0e8903d2f4f32841ec5667
Mehemmed476 <mehemmedxelilzade11@gmail.com>
2024-10-01 21:52:35 +0400

    University point finder

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿float gpa = 83.5f;
+
+switch (gpa)
+{
+    case float n when (n >= 51.0f && n < 61.0f):
+        Console.WriteLine("E");
+        break;
+    case float n when (n >= 61.0f && n < 71.0f):
+        Console.WriteLine("D");
+        break;
+    case float n when (n >= 71.0f && n < 81.0f):
+        Console.WriteLine("C");
+        break;
+    case float n when (n >= 81.0f && n < 91.0f):
+        Console.WriteLine("B");
+        break;
+    case float n when (n >= 91.0f && n <= 100.0f):
+        Console.WriteLine("A");
+        break;
+    default:
+        Console.WriteLine("you are disqualified");
+        break;
+}
